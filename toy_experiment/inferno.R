bff0e3727323a3057a6369ef29beb23704820a86
choisant <agre@hvl.no>
2024-08-31 10:42:03 +0200

    make data notebook much better

--- toy_experiment/inferno.R
+++ toy_experiment/inferno.R
@@ -3,14 +3,13 @@ set.seed(120)
 inferno_dir <- "inferno/run"
 if(!dir.exists(inferno_dir)) {dir.create((inferno_dir))}
 caldata <- "data/cal.csv"
-testdata <- "data/test.csv"
-#buildmetadata(caldata, file = "inferno/temp_metadata")
-metadata <- "inferno/metadata.csv"
+buildmetadata(caldata, file = "inferno/temp_metadata")
+stop()
+#metadata <- "inferno/metadata.csv"
 
-NSAMPLES <- 32
-NCHAINS <- 1
-NTRAIN <- 10
-NCORES <- 1
+NSAMPLES <- 3200
+NCHAINS <- 12
+NCORES <- 12
 
 
 # Start inference
@@ -22,5 +21,5 @@ learnt <- learn(
     nchains = NCHAINS,
     parallel = NCORES,
     appendtimestamp = FALSE,
-    appendinfo = FALSE
+    appendinfo = TRUE
 )
