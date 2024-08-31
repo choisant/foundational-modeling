3e1a9e29dfe0a40503b5d9f1014bd345f7aa1f74
choisant <agre@hvl.no>
2024-08-31 09:15:41 +0200

    Inferno run working

--- toy_experiment/inferno.R
+++ toy_experiment/inferno.R
@@ -5,7 +5,7 @@ if(!dir.exists(inferno_dir)) {dir.create((inferno_dir))}
 caldata <- "data/cal.csv"
 testdata <- "data/test.csv"
 #buildmetadata(caldata, file = "inferno/temp_metadata")
-metadata <- "inferno/debug_metadata.csv"
+metadata <- "inferno/metadata.csv"
 
 NSAMPLES <- 32
 NCHAINS <- 1
